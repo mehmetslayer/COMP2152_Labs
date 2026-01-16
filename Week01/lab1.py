c03db36961fcb238766797acae6392d1328abb6e
mehmetslayer <eminonem@hotmail.com>
2026-01-16 12:34:47 -0500

    add lab 01 done

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -3,6 +3,34 @@
 
 #Question 2 defining an array (list)
 
-input("What's the input of array elements?  ")
+magic_array = [1 , 3 , 4 , 9 ]
 
-my_array = [1 , 4 , 7 , 9]
+#Question 3: Order of Operations 
+
+a = 1 
+b = 2
+c = 3
+d = 4 
+
+# fully bracketed version of the expression 
+
+e = (a - ((b ** c) // d)) + (a % c)
+
+# The final value of e is 0
+
+print(f"the value of e is {e}")
+
+# Question 4: String formatting
+# Create a variable called “temperature” with the value 32.6. Then, print the following
+#sentence by using the variable and the format function, with 1 line of code.“The temperature
+#today is: 32.600 degrees Celsius
+
+temperature = 32.6
+print(f"The temperature today is : {temperature:.3f} degrees celcius")
+
+#Question 5: User Input
+
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+
+print (f"Now showing the shop items filtred by age {userAge}")
