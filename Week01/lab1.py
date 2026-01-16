7917f3b36d6796d5cb53df9d44e258c130192717
mehmetslayer <eminonem@hotmail.com>
2026-01-16 11:47:47 -0500

    add lab01

---
+++ Week01/lab1.py
@@ -0,0 +1,8 @@
+# Sample Coding Question 01 Week 01 
+# Mehmet Emin Onem 
+
+#Question 2 defining an array (list)
+
+input("What's the input of array elements?  ")
+
+my_array = [1 , 4 , 7 , 9]
---
+++ Week01/q5.py
@@ -0,0 +1,3 @@
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print (f"Now showing the shop items filtred by age {userAge}")
